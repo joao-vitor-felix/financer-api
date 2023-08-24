b0af2112e184e14739f86a6a55d838126f8352cb
João Vitor <joaovitorfelixcontanto@gmail.com>
2023-08-24 09:57:47 -0300

    feat: added PostgresGetUserById repository

---
+++ src/repositories/postgres/get-user-by-id.js
@@ -0,0 +1,12 @@
+import { PostgresHelper } from "../../db/postgres/helper.js";
+
+export class PostgresGetUserById {
+  async getUserById(id) {
+    const userReturned = await PostgresHelper.query(
+      "SELECT * FROM users WHERE id = $1",
+      [id]
+    );
+
+    return userReturned?.[0];
+  }
+}
