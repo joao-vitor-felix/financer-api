d7dd9c1e1473a20f54720144a30516661060869d
João Vitor <joaovitorfelixcontato@gmail.com>
2023-10-06 11:48:13 -0300

    feat: added update user route

--- index.js
+++ index.js
@@ -2,21 +2,28 @@ import "dotenv/config.js";
 import express from "express";
 import { CreateUserController } from "./src/controllers/create-user.js";
 import { GetUserByIdController } from "./src/controllers/get-user-by-id.js";
+import { UpdateUserController } from "./src/controllers/update-user.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/users", async (request, response) => {
+app.post("/api/users", async (request, response) => {
   const createUserController = new CreateUserController();
   const { statusCode, body } = await createUserController.createUser(request);
   return response.status(statusCode).send(body);
 });
 
-app.get(`/users/:id`, async (request, response) => {
+app.get(`/api/users/:userId`, async (request, response) => {
   const getUserByIdController = new GetUserByIdController();
   const { statusCode, body } = await getUserByIdController.getUserById(request);
   return response.status(statusCode).send(body);
+});
+
+app.patch("/api/users/:userId", async (request, response) => {
+  const updateUserController = new UpdateUserController();
+  const { statusCode, body } = await updateUserController.updateUser(request);
+  response.status(statusCode).send(body);
 });
 
 app.listen(process.env.PORT, () =>
