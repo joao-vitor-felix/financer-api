cdd20b7f270add4c8aa49646f142234b16d181b1
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2023-10-11 11:12:02 -0300

    feat: added PostgresDeleteUserRepository

---
+++ src/repositories/postgres/delete-user.js
@@ -0,0 +1,12 @@
+import { PostgresHelper } from "../../db/postgres/helper.js";
+
+export class PostgresDeleteUserRepository {
+  async deleteUser(id) {
+    const userDeleted = await PostgresHelper.query(
+      "DELETE FROM users WHERE id = $1 RETURNING *",
+      [id]
+    );
+
+    return userDeleted?.[0];
+  }
+}
