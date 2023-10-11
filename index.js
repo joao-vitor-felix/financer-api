a08c337515a0c0b4304d343c8a5c22f734b3934f
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2023-10-11 11:14:04 -0300

    feat: added delete route

--- index.js
+++ index.js
@@ -3,18 +3,13 @@ import express from "express";
 import {
   CreateUserController,
   GetUserByIdController,
-  UpdateUserController
+  UpdateUserController,
+  DeleteUserController
 } from "./src/controllers/index.js";
 
 const app = express();
 
 app.use(express.json());
-
-app.post("/api/users", async (request, response) => {
-  const createUserController = new CreateUserController();
-  const { statusCode, body } = await createUserController.createUser(request);
-  return response.status(statusCode).send(body);
-});
 
 app.get(`/api/users/:userId`, async (request, response) => {
   const getUserByIdController = new GetUserByIdController();
@@ -22,9 +17,21 @@ app.get(`/api/users/:userId`, async (request, response) => {
   return response.status(statusCode).send(body);
 });
 
+app.post("/api/users", async (request, response) => {
+  const createUserController = new CreateUserController();
+  const { statusCode, body } = await createUserController.createUser(request);
+  return response.status(statusCode).send(body);
+});
+
 app.patch("/api/users/:userId", async (request, response) => {
   const updateUserController = new UpdateUserController();
   const { statusCode, body } = await updateUserController.updateUser(request);
+  response.status(statusCode).send(body);
+});
+
+app.delete("/api/users/:userId", async (request, response) => {
+  const deleteUserController = new DeleteUserController();
+  const { statusCode, body } = await deleteUserController.deleteUser(request);
   response.status(statusCode).send(body);
 });
 
