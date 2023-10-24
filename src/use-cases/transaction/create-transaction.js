0dfd30134e13342bc73cbf16776c5265b1667947
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2023-10-24 09:59:50 -0300

    feat: add CreateTransactionUseCase

---
+++ src/use-cases/transaction/create-transaction.js
@@ -0,0 +1,28 @@
+import { v4 as uuidv4 } from "uuid";
+import { UserNotFoundError } from "../../errors/user.js";
+
+export class CreateTransactionUseCase {
+  constructor(createTransactionRepository, getUserByIdRepository) {
+    this.createTransactionRepository = createTransactionRepository;
+    this.getUserByIdRepository = getUserByIdRepository;
+  }
+
+  async createTransaction(createTransactionParams) {
+    const userId = createTransactionParams.userId;
+
+    const user = this.getUserByIdRepository.getUserById(userId);
+
+    if (!user) {
+      throw new UserNotFoundError(userId);
+    }
+
+    const transactionId = uuidv4();
+    const transaction =
+      await this.createTransactionRepository.createTransaction({
+        ...createTransactionParams,
+        id: transactionId
+      });
+
+    return transaction;
+  }
+}
