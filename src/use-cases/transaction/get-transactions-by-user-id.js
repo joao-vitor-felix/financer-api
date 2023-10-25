f783c8f36dcd37e57c63878ba722094a89f4c363
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2023-10-25 10:48:29 -0300

    feat: add GetTransactionsByUserIdUseCase

---
+++ src/use-cases/transaction/get-transactions-by-user-id.js
@@ -0,0 +1,25 @@
+import { UserNotFoundError } from "../../errors/user.js";
+
+export class GetTransactionsByUserIdUseCase {
+  constructor(getTransactionsByUserIdRepository, getUserByIdRepository) {
+    this.getTransactionsByUserIdRepository = getTransactionsByUserIdRepository;
+    this.getUserByIdRepository = getUserByIdRepository;
+  }
+
+  async getTransactions(params) {
+    const user = await this.getUserByIdRepository.getTransactions(
+      params.userId
+    );
+
+    if (!user) {
+      throw new UserNotFoundError(params.userId);
+    }
+
+    const transactions =
+      await this.getTransactionsByUserIdRepository.getTransactions(
+        params.userId
+      );
+
+    return transactions;
+  }
+}
