336cf0d5e3f998204de71884accd924f5489773d
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2023-10-25 11:04:24 -0300

    feat: add PostgresUpdateTransactionRepository

---
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -0,0 +1,26 @@
+import { PostgresHelper } from "../../../db/postgres/helper.js";
+
+export class PostgresUpdateTransactionRepository {
+  async updateTransaction(userId, updateTransactionParams) {
+    const updateFields = [];
+    const updateValues = [];
+
+    Object.keys(updateTransactionParams).forEach(key => {
+      updateFields.push(`${key} = $${updateValues.length + 1}`);
+      updateValues.push(updateTransactionParams[key]);
+    });
+
+    updateValues.push(userId);
+
+    const updateQuery = `
+            UPDATE transactions
+            SET ${updateFields.join(", ")} 
+            WHERE id = $${updateValues.length}
+            RETURNING *
+        `;
+
+    const updatedUser = await PostgresHelper.query(updateQuery, updateValues);
+
+    return updatedUser[0];
+  }
+}
