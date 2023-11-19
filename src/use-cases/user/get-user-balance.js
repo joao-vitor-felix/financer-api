f059c2b807465a6370ac993cd7a7aab933881df0
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2023-11-19 11:00:15 -0300

    feat: add GetUserBalanceUseCase

---
+++ src/use-cases/user/get-user-balance.js
@@ -0,0 +1,21 @@
+import { UserNotFoundError } from "../../errors/user";
+
+export class GetUserBalanceUseCase {
+  constructor(getUserBalanceRepository, getUserIdRepository) {
+    this.getUserBalanceRepository = getUserBalanceRepository;
+    this.getUserIdRepository = getUserIdRepository;
+  }
+
+  async getUserBalance(userId) {
+    const user = await this.getUserIdRepository.getUserId(userId);
+
+    if (!user) {
+      throw new UserNotFoundError(userId);
+    }
+
+    const userBalance =
+      await this.getUserBalanceRepository.getUserBalance(userId);
+
+    return userBalance;
+  }
+}
