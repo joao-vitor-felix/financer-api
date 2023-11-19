c5f6b56b70435a451f04d76724e962bbcbbf0643
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2023-11-19 11:29:12 -0300

    fix: syntax error on PostgresGetUserBalanceRepository

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -3,7 +3,7 @@ import { PostgresHelper } from "../../../db/postgres/helper.js";
 export class PostgresGetUserBalanceRepository {
   async getUserBalance(userId) {
     const userBalance = await PostgresHelper.query(
-      `SELECT 
+      `SELECT
             SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END) AS earnings,
             SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END) AS expenses,
             SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END) AS investments,
@@ -11,7 +11,7 @@ export class PostgresGetUserBalanceRepository {
             - SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END)
             - SUM (CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END)) AS balance
         FROM transactions
-        WHERE user_id = '$1'`,
+        WHERE user_id = $1`,
       [userId]
     );
 
