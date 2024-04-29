c365d37ac0d4fd81f1f4a60c42cc76ce8d659ecb
João Vitor <joaovitorfelixcontato@gmail.com>
2024-04-29 18:01:33 -0300

    feat: add strict to createUserSchema

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -1,32 +1,36 @@
 import z from "zod";
 
-export const createUserSchema = z.object({
-  firstName: z
-    .string({ required_error: "First name not provided." })
-    .trim()
-    .min(1, {
-      message: "First name must contain at least 1 character."
-    }),
-  lastName: z
-    .string({ required_error: "Last name not provided." })
-    .trim()
-    .min(1, {
-      message: "Last name must contain at least 1 character."
-    }),
-  email: z
-    .string({ required_error: "Email not provided." })
-    .email({ message: "Please, provide a valid email." })
-    .trim()
-    .min(1, {
-      message: "Please, provide an email."
-    }),
-  password: z
-    .string({ required_error: "Password not provided." })
-    .trim()
-    .min(6, {
-      message: "Password must contain at least 6 characters."
-    })
-});
+export const createUserSchema = z
+  .object({
+    firstName: z
+      .string({ required_error: "First name not provided." })
+      .trim()
+      .min(1, {
+        message: "First name must contain at least 1 character."
+      }),
+    lastName: z
+      .string({ required_error: "Last name not provided." })
+      .trim()
+      .min(1, {
+        message: "Last name must contain at least 1 character."
+      }),
+    email: z
+      .string({ required_error: "Email not provided." })
+      .email({ message: "Please, provide a valid email." })
+      .trim()
+      .min(1, {
+        message: "Please, provide an email."
+      }),
+    password: z
+      .string({ required_error: "Password not provided." })
+      .trim()
+      .min(6, {
+        message: "Password must contain at least 6 characters."
+      })
+  })
+  .strict({
+    message: "Some provided field is not allowed."
+  });
 
 export const updateUserSchema = createUserSchema.partial().strict({
   message: "Some provided field is not allowed."
