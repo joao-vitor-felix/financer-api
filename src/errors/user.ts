827f5bc56de53179013a9da0ccc385771f016ccc
João Vitor <joaovitorfelixcontato@gmail.com>
2024-05-07 13:31:51 -0300

    refactor: convert user errors to ts

--- src/errors/user.ts
+++ src/errors/user.ts
@@ -1,12 +1,12 @@
 export class EmailAlreadyInUseError extends Error {
-  constructor(email) {
+  constructor(email: string) {
     super(`The e-mail ${email} is already in use.`);
     this.name = "EmailAlreadyInUseError";
   }
 }
 
 export class UserNotFoundError extends Error {
-  constructor(userId) {
+  constructor(userId: string) {
     super(`The user with id ${userId} was not found.`);
     this.name = "UserNotFoundError";
   }
