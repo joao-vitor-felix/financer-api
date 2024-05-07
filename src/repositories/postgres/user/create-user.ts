3210cbb95519b87750561b1338c74bad525f2154
João Vitor <joaovitorfelixcontato@gmail.com>
2024-05-07 15:54:40 -0300

    fix: make sure to receive password on PostgresCreateUserRepository

--- src/repositories/postgres/user/create-user.ts
+++ src/repositories/postgres/user/create-user.ts
@@ -1,8 +1,12 @@
 import { CreateUserParams, ICreateUserRepository } from "@/types";
 import { prisma } from "../../../../prisma/client";
 
+interface CreateUserRepositoryParams extends CreateUserParams {
+  password: string;
+}
+
 export class PostgresCreateUserRepository implements ICreateUserRepository {
-  async createUser(params: CreateUserParams) {
+  async createUser(params: CreateUserRepositoryParams) {
     const user = await prisma.user.create({
       data: params
     });
