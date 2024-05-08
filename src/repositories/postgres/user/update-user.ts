2c8614c0f2340c6769ea35aead3f65c9c2454e74
João Vitor <joaovitorfelixcontato@gmail.com>
2024-05-08 14:43:31 -0300

    refactor: rename updateUserParams to params alongside import correct typing on UpdateUserRepository

--- src/repositories/postgres/user/update-user.ts
+++ src/repositories/postgres/user/update-user.ts
@@ -1,14 +1,15 @@
-import { IUpdateUserRepository, UpdateUserParams } from "@/types";
+import { UpdateUserSchema } from "@/schemas";
+import { IUpdateUserRepository } from "@/types";
 
 import { prisma } from "../../../../prisma/client";
 
 export class PostgresUpdateUserRepository implements IUpdateUserRepository {
-  async updateUser(userId: string, updateUserParams: UpdateUserParams) {
+  async updateUser(userId: string, params: UpdateUserSchema) {
     const user = await prisma.user.update({
       where: {
         id: userId
       },
-      data: updateUserParams
+      data: params
     });
 
     return user;
