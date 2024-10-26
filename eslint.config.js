601879006a58d5280a4845116fb3e1eb11cd24a2
João Vitor <joaovitorfelixcontato@gmail.com>
2024-10-26 19:26:58 -0300

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,17 +3,17 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintConfigPrettier,
-  {
-    plugins: {
-      "simple-import-sort": simpleImportSort
-    },
-    rules: {
-      "simple-import-sort/imports": "warn",
-      "simple-import-sort/exports": "warn"
-    }
+export default tseslint.config({
+  extends: [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintConfigPrettier
+  ],
+  plugins: {
+    "simple-import-sort": simpleImportSort
+  },
+  rules: {
+    "simple-import-sort/imports": "warn",
+    "simple-import-sort/exports": "warn"
   }
-);
+});
