54e9f6c8f557399aa8472ca1f1d4565d8c9fb210
João Vitor <joaovitorfelixcontato@gmail.com>
2024-12-21 12:16:43 -0300

    chore: update scripts alongside adding commitlint

--- src/main.ts
+++ src/main.ts
@@ -75,6 +75,16 @@ app.delete("/api/transactions/:transactionId", async (request, response) => {
   response.status(statusCode).send(body);
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(` Server running on port ${process.env.PORT}`)
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
 );
+
+const shutdown = () => {
+  console.log("Shutting down server...");
+  server.close(() => {
+    console.log("Server is shut down");
+    process.exit(0);
+  });
+};
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
