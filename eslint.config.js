299585b0ac60b4e01ad34e976ad285878fd033ee
João Vitor <joaovitorfelixcontato@gmail.com>
2025-03-09 18:23:24 -0300

    chore: turn no-explicit-any rule off from eslint

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,7 @@ export default tseslint.config({
   },
   rules: {
     "simple-import-sort/imports": "warn",
-    "simple-import-sort/exports": "warn"
+    "simple-import-sort/exports": "warn",
+    "@typescript-eslint/no-explicit-any": "off"
   }
 });
