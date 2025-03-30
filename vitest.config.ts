977ef6078d95a1dbccb42c845c03635bd550a773
João Vitor <joaovitorfelixcontato@gmail.com>
2025-03-30 18:19:18 -0300

    chore: add configs to vitest, remove flags from test scripts

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    globals: true
+    globals: true,
+    silent: true,
+    passWithNoTests: true
   },
   resolve: {
     alias: {
