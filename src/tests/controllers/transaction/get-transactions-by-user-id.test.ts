6bf2d2955b7282897ad2afe02080cd8619a6c50a
João Vitor <joaovitorfelixcontato@gmail.com>
2025-04-15 21:20:36 -0300

    test(GetTransactionsByUserIdController): should return 200 alongside user transactions

---
+++ src/tests/controllers/transaction/get-transactions-by-user-id.test.ts
@@ -0,0 +1,77 @@
+import { faker } from "@faker-js/faker";
+import { Transaction } from "@prisma/client";
+import { Decimal } from "@prisma/client/runtime/library";
+import type { Request } from "express";
+
+import { GetTransactionsByUserIdController } from "@/controllers";
+import { GetTransactionsByUserIdUseCase } from "@/use-cases";
+
+describe("GetTransactionsByUserIdController", () => {
+  class GetTransactionsByUserIdUseCaseStub {
+    async execute(userId: string): Promise<Transaction[]> {
+      return [
+        {
+          id: faker.string.uuid(),
+          userId,
+          date: faker.date.recent(),
+          amount: new Decimal(faker.number.int()),
+          name: faker.commerce.productName(),
+          type: "EXPENSE"
+        },
+        {
+          id: faker.string.uuid(),
+          userId,
+          date: faker.date.recent(),
+          amount: new Decimal(faker.number.int()),
+          name: faker.commerce.productName(),
+          type: "EARNING"
+        }
+      ];
+    }
+  }
+
+  function makeSut() {
+    const getTransactionsByUserIdUseCase =
+      new GetTransactionsByUserIdUseCaseStub() as GetTransactionsByUserIdUseCase;
+    const sut = new GetTransactionsByUserIdController(
+      getTransactionsByUserIdUseCase
+    );
+
+    return {
+      getTransactionsByUserIdUseCase,
+      sut
+    };
+  }
+
+  const httpRequest = {
+    query: {
+      userId: faker.string.uuid()
+    }
+  } as Request<any, any, any, { userId: string }>;
+
+  it("should return 200 alongside user transactions", async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute(httpRequest);
+
+    expect(result.statusCode).toBe(200);
+    expect(result.body).toContainEqual<Transaction>({
+      id: expect.any(String),
+      userId: expect.any(String),
+      date: expect.any(Date),
+      amount: expect.any(Decimal),
+      name: expect.any(String),
+      type: expect.any(String)
+    });
+  });
+  it.todo("should return 400 if userId is not provided", async () => {});
+  it.todo("should return 400 if userId is not a valid id", async () => {});
+  it.todo(
+    "should return 404 if GetTransactionsByUserIdUseCase throws UserNotFoundError",
+    async () => {}
+  );
+  it.todo(
+    "should return 500 if GetTransactionsByUserIdUseCase throws an unknown error",
+    async () => {}
+  );
+});
