d22093c45019cec5651a1f7d3129ac9e68d508a1
João Vitor <joaovitorfelixcontato@gmail.com>
2025-05-16 14:50:50 -0300

    fix: make sure id is an uuid on user fixture

--- src/tests/fixtures/user.ts
+++ src/tests/fixtures/user.ts
@@ -1,7 +1,7 @@
 import { faker } from "@faker-js/faker";
 
 export const user = {
-  id: faker.person.firstName(),
+  id: faker.string.uuid(),
   firstName: faker.person.firstName(),
   lastName: faker.person.lastName(),
   email: faker.internet.email(),
