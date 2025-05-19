e2e39d9fceca605605251617586a56c3de871589
João Vitor <joaovitorfelixcontato@gmail.com>
2025-05-19 10:08:43 -0300

    test(GetTransactionsByUserIdUseCase): should retrieve user transaction successfully

---
+++ src/tests/use-cases/transaction/get-transactions-by-user-id.test.ts
@@ -0,0 +1,73 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { faker } from "@faker-js/faker";
+import { Transaction } from "@prisma/client";
+import { Decimal } from "@prisma/client/runtime/library";
+
+import { GetUserByIdRepositoryStub } from "@/tests/stubs/GetUserByIdRepositoryStub";
+import { GetTransactionsByUserIdUseCase } from "@/use-cases";
+
+describe("GetTransactionsByUserIdUseCase", () => {
+  const userId = faker.string.uuid();
+  const transactions: Transaction[] = [
+    {
+      id: faker.string.uuid(),
+      userId,
+      name: faker.commerce.productName(),
+      date: faker.date.recent().toISOString() as unknown as Date,
+      type: "EXPENSE",
+      amount: new Decimal(faker.number.int())
+    }
+  ];
+
+  class GetTransactionsByUserIdRepositoryStub {
+    async execute(_userId: string): Promise<Transaction[]> {
+      return transactions;
+    }
+  }
+
+  function makeSut() {
+    const getTransactionsByUserIdRepository =
+      new GetTransactionsByUserIdRepositoryStub();
+    const getUserByIdRepository = new GetUserByIdRepositoryStub();
+    const sut = new GetTransactionsByUserIdUseCase(
+      getTransactionsByUserIdRepository,
+      getUserByIdRepository
+    );
+
+    return {
+      sut,
+      getTransactionsByUserIdRepository,
+      getUserByIdRepository
+    };
+  }
+
+  it("should retrieve user transaction successfully", async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute(userId);
+
+    expect(result).toEqual(transactions);
+  });
+
+  it.todo(
+    "should call GetUserByIdRepository with correct param",
+    async () => {}
+  );
+
+  it.todo(
+    "should throw UserNotFoundError if GetUserByIdRepository returns no user",
+    async () => {}
+  );
+
+  it.todo(
+    "should call GetTransactionsByUserIdRepository with correct param",
+    async () => {}
+  );
+
+  it.todo("should throw if GetUserByIdRepository throws", async () => {});
+
+  it.todo(
+    "should throw if GetTransactionsByUserIdRepository throws",
+    async () => {}
+  );
+});
