b28e2f6c2fe91d3b007d0fa0f1cd1bd4c9325557
João Vitor <joaovitorfelixcontato@gmail.com>
2025-05-19 09:08:17 -0300

    test(CreateTransactionUseCase): should create a transaction successfully

---
+++ src/tests/use-cases/transaction/create-transaction.test.ts
@@ -0,0 +1,69 @@
+import { faker } from "@faker-js/faker";
+
+import { CreateTransactionSchema } from "@/schemas";
+import { GetUserByIdRepositoryStub } from "@/tests/stubs/GetUserByIdRepositoryStub";
+import { CreateTransactionUseCase } from "@/use-cases";
+
+describe("CreateTransactionUseCase", () => {
+  class CreateTransactionRepositoryStub {
+    async execute(transaction: any) {
+      return {
+        ...transaction,
+        id: faker.string.uuid()
+      };
+    }
+  }
+
+  function makeSut() {
+    const createTransactionRepository = new CreateTransactionRepositoryStub();
+    const getUserByIdRepository = new GetUserByIdRepositoryStub();
+    const sut = new CreateTransactionUseCase(
+      createTransactionRepository,
+      getUserByIdRepository
+    );
+
+    return {
+      sut,
+      createTransactionRepository,
+      getUserByIdRepository
+    };
+  }
+
+  const transaction: CreateTransactionSchema = {
+    userId: faker.string.uuid(),
+    name: faker.commerce.productName(),
+    date: faker.date.anytime().toISOString(),
+    type: "EXPENSE",
+    amount: faker.number.int()
+  };
+
+  it("should create a transaction successfully", async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute(transaction);
+
+    expect(result).toEqual({
+      id: expect.any(String),
+      ...transaction
+    });
+  });
+
+  it.todo(
+    "should call GetUserByIdRepository with correct param",
+    async () => {}
+  );
+
+  it.todo(
+    "should throw UserNotFoundError GetUserByIdRepository with correct param",
+    async () => {}
+  );
+
+  it.todo(
+    "should call CreateTransactionRepository with correct params",
+    async () => {}
+  );
+
+  it.todo("should throw if CreateTransactionRepository throws", async () => {});
+
+  it.todo("should throw if GetUserByIdRepository throws", async () => {});
+});
