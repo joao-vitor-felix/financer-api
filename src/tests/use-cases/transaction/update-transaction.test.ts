f3ca946f32edb768e2e58b3f9b12f0a22f20124a
João Vitor <joaovitorfelixcontato@gmail.com>
2025-05-19 11:27:06 -0300

    test(UpdateTransactionUseCase): should update a transaction successfully

---
+++ src/tests/use-cases/transaction/update-transaction.test.ts
@@ -0,0 +1,70 @@
+import { faker } from "@faker-js/faker";
+import { Transaction } from "@prisma/client";
+import { Decimal } from "@prisma/client/runtime/library";
+
+import { UpdateTransactionSchema } from "@/schemas";
+import { UpdateTransactionUseCase } from "@/use-cases";
+
+describe("UpdateTransactionUseCase", () => {
+  const transactionId = faker.string.uuid();
+
+  const transaction: Transaction = {
+    id: transactionId,
+    userId: faker.string.uuid(),
+    name: faker.commerce.productName(),
+    date: faker.date.recent().toISOString() as unknown as Date,
+    type: "EXPENSE",
+    amount: new Decimal(faker.number.int())
+  };
+
+  class UpdateTransactionRepositoryStub {
+    async execute(transactionId: string, params: UpdateTransactionSchema) {
+      return {
+        id: transactionId,
+        userId: transaction.userId,
+        name: params.name ?? transaction.name,
+        date: (params.date as unknown as Date) ?? transaction.date,
+        type: params.type ?? transaction.type,
+        amount: params.amount ? new Decimal(params.amount) : transaction.amount
+      };
+    }
+  }
+
+  function makeSut() {
+    const updateTransactionRepository = new UpdateTransactionRepositoryStub();
+    const sut = new UpdateTransactionUseCase(updateTransactionRepository);
+
+    return {
+      sut,
+      updateTransactionRepository
+    };
+  }
+
+  it("should update a transaction successfully", async () => {
+    const { sut } = makeSut();
+
+    const params: UpdateTransactionSchema = {
+      name: "Tiger",
+      type: "EARNING",
+      amount: 1000
+    };
+
+    const result = await sut.execute(transactionId, params);
+
+    expect(result).toEqual({
+      id: transactionId,
+      userId: transaction.userId,
+      name: params.name,
+      date: transaction.date,
+      type: params.type,
+      amount: new Decimal(params.amount!)
+    });
+  });
+
+  it.todo(
+    "should call UpdateTransactionRepository with correct params",
+    async () => {}
+  );
+
+  it.todo("should throw if UpdateTransactionRepository throws", async () => {});
+});
