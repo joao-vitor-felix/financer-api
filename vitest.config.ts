dc6a2339188acb32d6278e13f13cf613fdba2fd0
João Vitor <joaovitorfelixcontato@gmail.com>
2025-05-20 10:45:34 -0300

    test: add env test file to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,16 @@
 import path from "node:path";
 
+import { config } from "dotenv";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
     silent: true,
-    passWithNoTests: true
+    passWithNoTests: true,
+    env: {
+      ...config({ path: ".env.test" }).parsed
+    }
   },
   resolve: {
     alias: {
