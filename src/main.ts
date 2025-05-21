98d82fc4b9b719f943454dbad704dd0257b83d95
João Vitor <joaovitorfelixcontato@gmail.com>
2025-05-21 20:43:18 -0300

    feat: add transactionsRouter

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,12 @@
 import express from "express";
 
-import {
-  makeCreateTransactionController,
-  makeDeleteTransactionController,
-  makeGetTransactionsByUserIdController,
-  makeUpdateTransactionController
-} from "@/factories";
-
-import { usersRouter } from "./routes/user";
+import { transactionsRouter, usersRouter } from "./routes";
 
 const app = express();
+
 app.use(express.json());
 app.use("/users", usersRouter);
-
-app.get("/transactions", async (request, response) => {
-  const getTransactionsByUserIdController =
-    makeGetTransactionsByUserIdController();
-  const { statusCode, body } =
-    await getTransactionsByUserIdController.execute(request);
-  response.status(statusCode).send(body);
-});
-
-app.post("/transactions", async (request, response) => {
-  const createTransactionController = makeCreateTransactionController();
-  const { statusCode, body } =
-    await createTransactionController.execute(request);
-  response.status(statusCode).send(body);
-});
-
-app.patch("/transactions/:transactionId", async (request, response) => {
-  const updateTransactionController = makeUpdateTransactionController();
-  const { statusCode, body } =
-    await updateTransactionController.execute(request);
-  response.status(statusCode).send(body);
-});
-
-app.delete("/transactions/:transactionId", async (request, response) => {
-  const deleteTransactionController = makeDeleteTransactionController();
-  const { statusCode, body } =
-    await deleteTransactionController.execute(request);
-  response.status(statusCode).send(body);
-});
+app.use("/transactions", transactionsRouter);
 
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
