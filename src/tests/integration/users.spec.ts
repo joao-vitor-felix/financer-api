6501dbe53bb60d0cacb4124f1e480146839e464a
João Vitor <joaovitorfelixcontato@gmail.com>
2025-05-22 14:31:08 -0300

    test(POST /users): should return 409 when email already in use is provided

--- src/tests/integration/users.spec.ts
+++ src/tests/integration/users.spec.ts
@@ -39,6 +39,14 @@ describe("Users integration tests", () => {
       expect(response.body.message).toMatch(/valid/i);
     });
 
+    it("should return 409 when email already in use is provided", async () => {
+      await request(app).post("/users").send(user);
+      const response = await request(app).post("/users").send(user);
+
+      expect(response.statusCode).toBe(409);
+      expect(response.body.message).toMatch(/already in use/i);
+    });
+
     it("should return 500 when a server error happens", async () => {
       vi.spyOn(prisma.user, "create").mockRejectedValueOnce(new Error());
       const response = await request(app).post("/users").send(user);
