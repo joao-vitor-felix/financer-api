9e854cedb4f9881530b4704712427f55119e9648
João Vitor <joaovitorfelixcontato@gmail.com>
2025-05-25 12:23:47 -0300

    feat: add omit hashedPassword to PostgresGetUserByIdRepository, update prisma client

--- src/repositories/postgres/user/get-user-by-id.ts
+++ src/repositories/postgres/user/get-user-by-id.ts
@@ -4,6 +4,9 @@ import { IGetUserByIdRepository } from "@/types";
 export class PostgresGetUserByIdRepository implements IGetUserByIdRepository {
   async execute(id: string) {
     const userReturned = await prisma.user.findUnique({
+      omit: {
+        hashedPassword: true
+      },
       where: {
         id
       }
