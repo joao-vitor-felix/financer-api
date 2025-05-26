8b074385f054aa8c6166283c5a09a8cd60a4897b
João Vitor <joaovitorfelixcontato@gmail.com>
2025-05-26 18:57:13 -0300

    fix: make sure the database is ready to accept connections on global-setup

--- src/tests/global-setup.ts
+++ src/tests/global-setup.ts
@@ -1,6 +1,7 @@
 import { execSync } from "child_process";
 
 export function setup() {
-  execSync("docker compose up postgres-test --wait -d");
+  execSync("docker compose up -d --wait postgres-test");
+  execSync("sleep 1");
   execSync("npx prisma db push");
 }
