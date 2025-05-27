575082a4d305bfcd0fd78882cd0359bbbea1e3bd
João Vitor <joaovitorfelixcontato@gmail.com>
2025-05-27 10:12:36 -0300

    chore: add initial swagger setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
+import swagger from "../docs/swagger.json";
 import { transactionsRouter, usersRouter } from "./routes";
 
 export const app = express();
@@ -7,3 +9,4 @@ export const app = express();
 app.use(express.json());
 app.use("/users", usersRouter);
 app.use("/transactions", transactionsRouter);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swagger));
